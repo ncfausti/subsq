b68d4852170fa0c347c4108ec3402ccca1d982e2
Nick Fausti <ncfausti@gmail.com>
2016-07-11 12:02:06 -0400

    iono?

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.http import HttpResponse
 
+@login_required
 def index(request):
 	return HttpResponse('hello world')
+
+def login(request):
+	return HttpResponse('login here')
